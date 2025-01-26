4a3a9ade8d19240f195de81f1ef3dd402faffaf0
Collin Collins <97001409+collincollins@users.noreply.github.com>
2025-01-26 02:16:56 -0500

    make build outDir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    outDir: 'build', // output directory
+    sourcemap: false,
+    rollupOptions: {
+    },
+  },
   plugins: [
     svelte(),
     react() // so it can handle .jsx files
