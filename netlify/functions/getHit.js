d4230d47f28f6f8d4aab5c66c9848452303537cc
Collin Collins <97001409+collincollins@users.noreply.github.com>
2025-01-26 02:06:20 -0500

    add environment variable to mongodb getHit.js and incrementHit.js

--- netlify/functions/getHit.js
+++ netlify/functions/getHit.js
@@ -1,55 +1,55 @@
-// // netlify/functions/getHit.js
+// netlify/functions/getHit.js
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_ENV_VAR_CAN_YOU_BEAT_THE_MARKET;
 
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-// let isConnected = false;
+let isConnected = false;
 
-// exports.handler = async (event, context) => {
-//   if (event.httpMethod !== 'GET') {
-//     return {
-//       statusCode: 405,
-//       body: JSON.stringify({ message: 'Method Not Allowed' }),
-//     };
-//   }
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'GET') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
 
-//   try {
-//     console.log("Function 'getHit' invoked.");
+  try {
+    console.log("Function 'getHit' invoked.");
 
-//     if (!isConnected) {
-//       console.log("Connecting to MongoDB...");
-//       await client.connect();
-//       isConnected = true;
-//       console.log("Connected to MongoDB.");
-//     }
+    if (!isConnected) {
+      console.log("Connecting to MongoDB...");
+      await client.connect();
+      isConnected = true;
+      console.log("Connected to MongoDB.");
+    }
 
-//     const database = client.db('dontbuythat');
-//     const hitsCollection = database.collection('hits');
+    const database = client.db('dontbuythat');
+    const hitsCollection = database.collection('hits');
 
-//     const hit = await hitsCollection.findOne({ _id: 'hitCounter' });
+    const hit = await hitsCollection.findOne({ _id: 'hitCounter' });
 
-//     const count = hit ? hit.count : 0;
+    const count = hit ? hit.count : 0;
 
-//     console.log(`Retrieved hit count: ${count}`);
+    console.log(`Retrieved hit count: ${count}`);
 
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ count }),
-//     };
-//   } catch (error) {
-//     console.error('Error in getHit function:', error);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ message: 'Internal Server Error' }),
-//     };
-//   }
-// };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ count }),
+    };
+  } catch (error) {
+    console.error('Error in getHit function:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
+};
