ce741f15e118718741fb347f137292930ae08f41
Collin Collins <97001409+collincollins@users.noreply.github.com>
2025-01-28 10:39:17 -0500

    dependency update; adjust mobile formatting

--- src/App.svelte
+++ src/App.svelte
@@ -326,6 +326,7 @@
   .header-card {
     font-size: 1em;
     padding-top: 5px;
+    margin-right: 8px;
     width: 80%; /* max-width override */
     max-width: 900px;
   }
@@ -372,7 +373,9 @@
   .chart-container {
     width: 90%;
     max-width: 700px;
-    min-height: 300px;
+    min-height: 295px;
+
+    margin-right: 8px;
 
     padding-left: 10px;
     padding-right: 0px;
@@ -399,6 +402,11 @@
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+
+  .results-details-card {
+    max-width: 80%;
+    margin-right: 8px;
   }
 
   /* Invalid Simulation Message Specific Styles */
@@ -572,7 +580,7 @@
 
   <!-- High Score Display -->
   {#if simulationEnded}
-    <div class="card">
+    <div class="card results-details-card">
       <h2>High Score</h2>
       <p>
         {highScorePlayer} has the most consecutive wins with {currentHighScore}
